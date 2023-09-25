9eae3aab9d35896c1699d46f4f18344f93ebe21e
Saurabh <saurabhsalunke91@gmail.com>
2023-09-26 00:02:30 +0530

    Init: get started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{width:"100vw",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <p>TechFusion 2.0</p>
+    </div>
+  )
+}
+
+export default App
